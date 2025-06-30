5e45caa5dc0cf536a69d5e752449404799c15d32
xunxxoie <ahh0520@naver.com>
2025-06-30 18:42:25 +0900

    feat: Initial Commit

---
+++ src/main/java/gravit/code/GravitApplication.java
@@ -0,0 +1,13 @@
+package gravit.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GravitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gravit/code/GravitApplicationTests.java
@@ -0,0 +1,13 @@
+package gravit.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
