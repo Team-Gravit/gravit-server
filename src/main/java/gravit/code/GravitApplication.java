1f4db5ecc00b284170fc75f23b203ae049a1a23a
xunxxoie <ahh0520@naver.com>
2025-07-01 22:17:57 +0900

    Revert "feat: 유저 앤티티 생성 - #1"
    This reverts commit 9cb4ccc108f24917ce36e95e4845827f63bd97f5.

--- src/main/java/gravit/code/GravitApplication.java
+++ src/main/java/gravit/code/GravitApplication.java
@@ -2,9 +2,7 @@ package gravit.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class GravitApplication {
 
