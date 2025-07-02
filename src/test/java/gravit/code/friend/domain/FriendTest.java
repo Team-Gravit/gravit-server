da5dcbf199ddc2fabbad891016e13eb7ebdf882e
xunxxoie <ahh0520@naver.com>
2025-07-02 15:25:14 +0900

    test: 챕터, 친구, 리그 테스트 코드 수정 (#1)

--- src/test/java/gravit/code/friend/domain/FriendTest.java
+++ src/test/java/gravit/code/friend/domain/FriendTest.java
@@ -3,14 +3,23 @@ package gravit.code.friend.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FriendTest {
 
     @DisplayName("유효한 데이터로 친구를 생성할 수 있다")
     @Test
     void createFriendWithAvailableDate(){
-        assertThatNoException().isThrownBy(() ->
-                Friend.create(1L,2L));
+
+        // given
+        Long followingId = 1L;
+        Long followerId = 2L;
+
+        // when
+        Friend friend = Friend.create(followingId, followerId);
+
+        // then
+        assertThat(friend.getFollowingId()).isEqualTo(followingId);
+        assertThat(friend.getFollowerId()).isEqualTo(followerId);
     }
 }
--- src/test/java/gravit/code/league/domain/LeagueTest.java
+++ src/test/java/gravit/code/league/domain/LeagueTest.java
@@ -3,15 +3,28 @@ package gravit.code.league.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LeagueTest {
 
     @DisplayName("유효한 데이터로 리그를 생성할 수 있다")
     @Test
     void createLeagueWithAvailableData(){
-        assertThatNoException().isThrownBy(() ->
-                League.create("브론즈", 0, 300, "https://example.com/iamge1.jpg"));
+
+        // given
+        String name = "브론즈";
+        Integer maxXp = 0;
+        Integer minYp = 0;
+        String leagueImgUrl = "https://example.com/league1.jpg";
+
+        // when
+        League league = League.create(name, maxXp, minYp, leagueImgUrl);
+
+        // then
+        assertThat(league.getName()).isEqualTo(name);
+        assertThat(league.getMaxXp()).isEqualTo(maxXp);
+        assertThat(league.getMinXp()).isEqualTo(minYp);
+        assertThat(league.getLeagueImgUrl()).isEqualTo(leagueImgUrl);
     }
 
 }
