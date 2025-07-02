b6b0b2217ea5d213969cc6cd9776826b026c6a76
xunxxoie <ahh0520@naver.com>
2025-07-02 15:42:53 +0900

    test: 유저 테스트 코드 수정 (#1)

--- src/test/java/gravit/code/user/domain/UserTest.java
+++ src/test/java/gravit/code/user/domain/UserTest.java
@@ -3,14 +3,27 @@ package gravit.code.user.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class UserTest {
 
     @DisplayName("유효한 데이터로 유저를 생성할 수 있다")
     @Test
     void createUserWithAvailableData(){
-        assertThatNoException().isThrownBy(() ->
-                User.create("test@inu.ac.kr", "test", "@f9s2w30", "https://example.com/iamge1.jpg"));
+
+        // given
+        String email = "test@inu.ac.kr";
+        String nickname = "test";
+        String handle = "@f9s2w30";
+        String profileImgUrl = "https://example.com/iamge1.jpg";
+
+        // when
+        User user = User.create(email, nickname, handle, profileImgUrl);
+
+        // that
+        assertThat(user.getEmail()).isEqualTo(email);
+        assertThat(user.getNickname()).isEqualTo(nickname);
+        assertThat(user.getHandle()).isEqualTo(handle);
+        assertThat(user.getProfileImgUrl()).isEqualTo(profileImgUrl);
     }
 }
