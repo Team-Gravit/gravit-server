4aff20ff9864872490938d998407a0d368b9fa43
sukangpunch <popora99@naver.com>
2025-08-03 22:02:51 +0900

    fix: 테스트 에러 해결

--- src/test/java/gravit/code/domain/friend/service/FriendServiceTest.java
+++ src/test/java/gravit/code/domain/friend/service/FriendServiceTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
