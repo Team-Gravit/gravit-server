723fc11151d5b06edc31669ed9dffda7bb3b2e67
xunxxoie <ahh0520@naver.com>
2025-09-13 15:17:48 +0900

    feat: 오늘의 미션 랜덤 추첨시, 난이도에 따라 당첨 확률 분배 (#97)

--- src/main/java/gravit/code/mission/util/MissionUtil.java
+++ src/main/java/gravit/code/mission/util/MissionUtil.java
@@ -2,6 +2,8 @@ package gravit.code.mission.util;
 
 import gravit.code.mission.domain.MissionType;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public final class MissionUtil {
@@ -9,10 +11,17 @@ public final class MissionUtil {
     private static final Random random = new Random();
     
     private MissionUtil(){}
-
+    
     public static MissionType getRandomMissionType(){
-        MissionType[] allMissions = MissionType.values();
-        int randomIndex = random.nextInt(allMissions.length);
-        return allMissions[randomIndex];
+        List<MissionType> ticketPool = new ArrayList<>();
+        
+        for (MissionType mission : MissionType.values()) {
+            for (int i = 0; i < mission.getTicket(); i++) {
+                ticketPool.add(mission);
+            }
+        }
+        
+        int randomIndex = random.nextInt(ticketPool.size());
+        return ticketPool.get(randomIndex);
     }
 }
