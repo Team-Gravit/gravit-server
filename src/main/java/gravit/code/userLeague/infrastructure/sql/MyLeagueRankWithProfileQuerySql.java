9b0a595dd616a48db708f51e4ff21cd269a91dab
sukangpunch <popora99@naver.com>
2025-10-08 17:00:42 +0900

    fix: MyLeagueRank 조회에서 삭제된 유저도 rank 에 포함되는 오류 수정

--- src/main/java/gravit/code/userLeague/infrastructure/sql/MyLeagueRankWithProfileQuerySql.java
+++ src/main/java/gravit/code/userLeague/infrastructure/sql/MyLeagueRankWithProfileQuerySql.java
@@ -11,29 +11,44 @@ public class MyLeagueRankWithProfileQuerySql {
      **/
     public static final String FIND_MY_RANK_WITH_PROFILE_SQL = """
             WITH me AS (
-              SELECT ul.user_id, ul.league_id, ul.league_point
+              SELECT
+                ul.user_id,
+                ul.league_id,
+                ul.league_point,
+                u.nickname,
+                u.profile_img_number,
+                u.xp,
+                u.level
               FROM user_league ul
+              JOIN users u ON u.id = ul.user_id
               WHERE ul.user_id = :userId
+                AND u.deleted_at IS NULL
             )
             SELECT
-            me.league_id AS league_id,
-            l.name AS league_name,
-            l.max_lp AS max_lp,
+              me.league_id   AS league_id,
+              l.name         AS league_name,
+              l.max_lp       AS max_lp,
               (
                 SELECT 1 + COUNT(*)
                 FROM user_league ul2
-                JOIN me ON me.league_id = ul2.league_id
-                WHERE (ul2.league_point > me.league_point)
-                   OR (ul2.league_point = me.league_point AND ul2.user_id < me.user_id)
+                WHERE ul2.league_id = me.league_id
+                  AND (
+                    ul2.league_point > me.league_point
+                    OR (ul2.league_point = me.league_point AND ul2.user_id < me.user_id)
+                  )
+                  AND EXISTS (
+                    SELECT 1 FROM users u2
+                    WHERE u2.id = ul2.user_id
+                      AND u2.deleted_at IS NULL
+                  )
               ) AS rank,
               me.user_id,
-              me.league_point AS lp,
-              u.nickname,
-              u.profile_img_number,
-              u.xp,
-              u.level
+              me.league_point      AS lp,
+              me.nickname,
+              me.profile_img_number,
+              me.xp,
+              me.level
             FROM me
-            JOIN users u ON u.id = me.user_id AND u.deleted_at IS NULL
-            JOIN league l ON l.id = me.league_id
+            JOIN league l ON l.id = me.league_id;
             """;
 }
