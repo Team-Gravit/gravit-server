48016d8d9719f42bd4430c8be1758687fb08a65a
xunxxoie <ahh0520@naver.com>
2025-10-12 15:31:36 +0900

    test: LessonService 단위 테스트 작성 (#197)

--- src/main/java/gravit/code/learning/service/LessonService.java
+++ src/main/java/gravit/code/learning/service/LessonService.java
@@ -14,12 +14,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class LessonService {
     private final LessonRepository lessonRepository;
 
+    /**
+     * TODO
+     * findLearningIdsByLessonId가 Optional.empty()를 반환한다는 것이 레슨 조회에 실패했다는 것을 의미할 수 있는가?
+     * Lesson Not Found보다 Invalid LessonId 이렇게 예외 처리를 하는 것이 더 좋아보인다.
+     */
     @Transactional(readOnly = true)
     public LearningIds getLearningIdsByLessonId(long lessonId){
         return lessonRepository.findLearningIdsByLessonId(lessonId)
                 .orElseThrow(() -> new RestApiException(CustomErrorCode.LESSON_NOT_FOUND));
     }
 
+    /**
+     * TODO
+     * 여기도 위와 동일함.
+     */
     public LearningAdditionalInfo getLearningAdditionalInfoByLessonId(long lessonId){
         return lessonRepository.findLearningAdditionalInfoByLessonId(lessonId)
                 .orElseThrow(() -> new RestApiException(CustomErrorCode.LESSON_NOT_FOUND));
