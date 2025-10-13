cddb24924c4f1a8a47bf1afd720036a5fdfc080c
xunxxoie <ahh0520@naver.com>
2025-10-13 12:57:10 +0900

    test: LearningEventListener 단위 테스트 작성 (#197)

--- src/main/java/gravit/code/learning/dto/StreakDto.java
+++ src/main/java/gravit/code/learning/dto/StreakDto.java
@@ -1,7 +1,17 @@
 package gravit.code.learning.dto;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+
+@Builder(access = AccessLevel.PRIVATE)
 public record StreakDto(
         int before,
         int after
 ) {
+    public static StreakDto of(int before, int after) {
+        return StreakDto.builder()
+                .before(before)
+                .after(after)
+                .build();
+    }
 }
