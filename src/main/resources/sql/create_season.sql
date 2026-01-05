9c69b337c62d88126123afd8a3040c905a6a2487
sukangpunch <popora99@naver.com>
2026-01-05 16:07:37 +0900

    refactor: 사용하지 않는 sql 패키지 제거
    * feat: cs-note 업데이트 내용 추가

--- src/main/resources/sql/create_season.sql
+++
@@ -1,9 +0,0 @@
-UPDATE season
-SET
-    season_key = '2025-W41',
-    starts_at = '2025-10-06 00:00:00',
-    ends_at = '2025-10-13 00:00:00',
-    status = 'ACTIVE',
-    tz = 'Asia/Seoul'
-WHERE id = 1
-  AND NOT EXISTS (SELECT 1 FROM season WHERE status = 'ACTIVE' AND id <> 1);
